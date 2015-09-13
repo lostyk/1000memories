ff15f84e3628c3884c32fa1ebf4eecf553f8e377
lostyk <lostyk7777>
2015-09-13 23:01:13 +0400

    15

--- src/com/seven/memories/Main.java
+++
@@ -1,8 +0,0 @@
-package com.seven.memories;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/info/teib/lab/lostyk/lab1/FirstApp.java
+++ src/info/teib/lab/lostyk/lab1/FirstApp.java
@@ -1,4 +1,4 @@
-package info.teib.lab.lostyk.lab1;
+package info.teib.lab.lostyk.lab1 ;
 
 /**
  * Created by Admin on 12.09.15.
@@ -7,7 +7,7 @@ public class FirstApp {
 
     public static void main(String [] args ) {
         System.out.println ("Hello");
-        System.out.println ( 6 + 5 + 9 );
+        System.out.println ( 1 + 2 + 3 + 4 + 5 );
 
     }
 }
