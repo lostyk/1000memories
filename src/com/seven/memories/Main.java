b3b51631deede4beaf47d6e585a69e59b4ea6a39
lostyk <lostyk7777>
2015-09-13 22:27:22 +0400

    Initial commit

---
+++ src/com/seven/memories/Main.java
@@ -0,0 +1,8 @@
+package com.seven.memories;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/info/teib/lab/lostyk/lab1/FirstApp.java
@@ -0,0 +1,13 @@
+package info.teib.lab.lostyk.lab1;
+
+/**
+ * Created by Admin on 12.09.15.
+ */
+public class FirstApp {
+
+    public static void main(String [] args ) {
+        System.out.println ("Hello");
+        System.out.println ( 6 + 5 + 9 );
+
+    }
+}
