4823f016be969e385a007f2cbf5d3fc059a5e9f3
lostyk <lostyk7777>
2015-09-13 23:05:35 +0400

    порахувала суму від 3 до 8

--- src/info/teib/lab/lostyk/lab1/FirstApp.java
+++ src/info/teib/lab/lostyk/lab1/FirstApp.java
@@ -8,6 +8,7 @@ public class FirstApp {
     public static void main(String [] args ) {
         System.out.println ("Hello");
         System.out.println ( 1 + 2 + 3 + 4 + 5 );
+        System.out.println( 3 + 4 + 5 + 6 + 7 + 8 );
 
     }
 }
