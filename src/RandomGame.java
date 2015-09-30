50668a85df8b17e33bf19997c08b05afba5c15c3
lostyk <lostyk7777>
2015-10-01 00:01:30 +0400

    нічого

---
+++ src/RandomGame.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Admin on 30.09.15.
+ */
+package com.uzhnu.training.samples.basics;
+
+import java.util.Random;
++
+
+public class RandomGame {
+
+    System.out.println( "Я задумав число від 0 до 100, спробуйте відгадати його?")
+}
